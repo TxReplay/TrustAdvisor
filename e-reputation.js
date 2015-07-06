db9b9240e5f4cb18ca393f1f419ef9ca90e3961e
Axel laurelut <axel.laurelut@cifacom.com>
2015-07-06 22:39:32 +0200

    add http package + api call

--- e-reputation.js
+++ e-reputation.js
@@ -1,5 +1,17 @@
 // Global access
-// travels = new Mongo.Collection( "travels" );
+test = new Mongo.Collection( "test" );
+
+if ( Meteor.isServer )
+{
+console.log(Meteor.http.call("GET", "https://www.kimonolabs.com/api/7amcc6em?apikey=YOkuRYZQhnkbwfVZCkl2auAoBOPVoQ7Y"));
+
+    // Meteor.methods({
+    //     checkTwitter: function () {
+    //         this.unblock();
+    //         return Meteor.http.call("GET", "http://search.twitter.com/search.json?q=perkytweets");
+    //     }
+    // });
+}
 
 if ( Meteor.isClient )
 {
