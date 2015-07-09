3b1d54cfa7ef5b2e7ba6f5d14ac27df1d97235fe
Mahinthan Kengatharan <mahin.than@hotmail.com>
2015-07-09 21:47:29 +0200

    added popup on discover page

--- client/discover.js
+++ client/discover.js
@@ -1,12 +1,41 @@
-	Template.discover.events
-	({
-		
-		'click #submit' : function ( e, t )
-		{
+Template.discover.events
+({
 
-			$( "#questionnaire" ).slideUp( "slow", function() {
-	        	$('#result').slideDown( "slow");
-			});
+  'click #submit': function (e, t) {
 
-		}
-	});
+    $("#questionnaire").slideUp("slow", function () {
+      $('#result').slideDown("slow");
+    });
+
+  },
+  'click .icon-share': function (e, t) {
+    var shareDialogInfo = {
+      template: Template.social_popup,
+      modalDialogClass: "custom-modal-dialog", //optional
+      modalBodyClass: "custom-modal-body", //optional
+      modalFooterClass: "custom-modal-footer",//optional
+      removeOnHide: true, //optional. If this is true, modal will be removed from DOM upon hiding
+      doc: {  // Provide data context for Template.appShareDialog
+        app: "My Application"
+      }
+    };
+
+    window.popupLogin = ReactiveModal.initDialog(shareDialogInfo);
+    window.popupLogin.show();
+  },
+  'click .icon-link': function (e, t) {
+    var shareDialogInfo = {
+      template: Template.visit_new_site,
+      modalDialogClass: "custom-modal-dialog", //optional
+      modalBodyClass: "custom-modal-body", //optional
+      modalFooterClass: "custom-modal-footer",//optional
+      removeOnHide: true, //optional. If this is true, modal will be removed from DOM upon hiding
+      doc: {  // Provide data context for Template.appShareDialog
+        app: "My Application"
+      }
+    };
+
+    window.popupLogin = ReactiveModal.initDialog(shareDialogInfo);
+    window.popupLogin.show();
+  }
+});
